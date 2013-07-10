372ef92d68f0b6c449bdca17977dfa0a9acbf7a0
Alex Horak <alexamh9@gmail.com>
2013-07-10 09:28:17 -0400

    Removed old inspector projects and cleared manifest file

--- inspector/ios-inspector.2013-06-26T13-59-23.842263/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7/fmdb.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/fmdb.m \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabase.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMResultSet.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabasePool.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabaseAdditions.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabaseQueue.h
--- inspector/ios-inspector.2013-06-26T13-59-23.842263/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/FMDatabaseQueue.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabaseQueue.m \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabaseQueue.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabase.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMResultSet.h \
-  /Users/alexhorak/forge-workspace/plugins/database/inspector/ios-inspector/ForgeModule/FMDatabasePool.h
